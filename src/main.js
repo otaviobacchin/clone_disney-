14d897b044d97a7f93c14fb218f8807fb512a522
Otavio Bacchin <otavio.bacchin@gmail.com>
2023-06-19 12:39:31 -0300

    criação do FAQ e footer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question')
     
     for (let i = 0; i< buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
@@ -11,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
             button.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openOrCloseAnswer);
+    }
 })
 
 function removeActiveButton() {
@@ -19,6 +24,13 @@ function removeActiveButton() {
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
+}
+
+function openOrCloseAnswer(elemento) {
+    const openClass = 'faq__questions__item--is-open';
+    const fatherElement = elemento.target.parentNode;
+
+    fatherElement.classList.toggle(openClass);
 }
 
 function hideAllTabs() {
